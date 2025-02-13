c880e61e82c797ca30bca543a0abf1065ab878a5
Annie-spaces <huangwanqi@u.nus.edu>
2025-02-13 17:43:13 +0800

    Using online url for snoring dataset. No reduction in frames

--- MFCC feature extration (from wav).py
+++ MFCC feature extration (from wav).py
@@ -1,28 +1,51 @@
+#version 1, local memory, original frames
+# import librosa
+# file_path = r"C:\Users\lulu\Downloads\human_voice.wav" # Replace with your file path
+# y, sr = librosa.load(file_path, sr=None)  # Load with original sampling rate
+# mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+# print(mfccs)
+
+# Plotting MFCC
+# import numpy as np
+# import matplotlib.pyplot as plt
+# plt.figure(figsize=(10, 4))
+# librosa.display.specshow(mfccs, x_axis="time", sr=sr)
+# plt.colorbar()
+# plt.title("MFCC Features")
+# plt.xlabel("Time")
+# plt.ylabel("MFCC Coefficients")
+# plt.show()
+# print("MFCC Shape:", mfccs.shape)  # (Number of MFCCs, Time Frames)
+
+
+#verison 2, online url, original frame
+import requests
 import librosa
-import librosa.display
-import numpy as np
-import matplotlib.pyplot as plt
+import io
+import soundfile as sf
+wav_url = "https://raw.githubusercontent.com/adrianagaler/Snoring-Detection/master/Snoring_Dataset_%4016000/snoring/1_0.wav"
+response = requests.get(wav_url)
+if response.status_code == 200:
+    y, sr = sf.read(io.BytesIO(response.content))  # Read the WAV file
+else:
+    raise Exception("Failed to download the audio file")
+mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+#y is waveform, sr is sampling rate(Hz), n_mfcc is number of MFCC coefficients
 
-# Load audio file
-file_path = r"C:\Users\lulu\Downloads\human_voice.wav" # Replace with your file path
-y, sr = librosa.load(file_path, sr=None)  # Load with original sampling rate
+# Step 3: Plot MFCC
+# import numpy as np
+# import matplotlib.pyplot as plt
+# plt.figure(figsize=(10, 4))
+# librosa.display.specshow(mfccs, x_axis="time", sr=sr)
+# plt.colorbar()
+# plt.title("MFCC Features")
+# plt.xlabel("Time")
+# plt.ylabel("MFCC Coefficients")
+# plt.show()
+#
+# Step 4: Print the shape of MFCC features
+# print("MFCC Shape:", mfccs.shape)  # (Number of MFCCs, Time Frames)
 
-# Extract MFCC features
-n_mfcc = 13  # Number of MFCC coefficients
-mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
-print(mfccs)
 
-'''
-# Display MFCCs
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(mfccs, x_axis="time", sr=sr)
-plt.colorbar(label="MFCC Coefficients")
-plt.title("MFCC Features")
-plt.xlabel("Time")
-plt.ylabel("MFCC Coefficients")
-plt.show()
 
-# Convert to NumPy array
-mfccs_array = np.array(mfccs)
-print("MFCC Shape:", mfccs_array.shape)  # (n_mfcc, time_frames)
-'''
+
